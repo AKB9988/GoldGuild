94dc689833fce75b89f741ad2d120e26050638f0
Abhishek Kumar Bhatt <bhattak9617@gmail.com>
2026-06-06 15:36:32 +0530

    Initial Project Setup

---
+++ src/main/java/com/finance/goldguild/GoldGuildApplication.java
@@ -0,0 +1,13 @@
+package com.finance.goldguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldGuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldGuildApplication.class, args);
+    }
+
+}
